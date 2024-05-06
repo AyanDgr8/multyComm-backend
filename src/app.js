73d0213658323cd0514436f217267661e9e34e2b
Ayan <ayandgr5@gmail.com>
2024-05-06 18:51:03 +0800

    backend made

---
+++ src/app.js
@@ -0,0 +1,27 @@
+// src/app.js
+
+
+import express from "express";
+import cors from "cors";
+import router from './routes/router.js';
+// import paymentRoute from "./routes/paymentRoutes.js";
+import { errorHandler, notFoundHandler } from './middlewares/errorHandling.js';
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+
+
+// Middleware for handling 404 errors
+app.use(notFoundHandler);
+
+// Middleware for handling errors
+app.use(errorHandler);
+
+export { app };
---
+++ src/routes/router.js
@@ -0,0 +1,38 @@
+// src/routes/router.js
+
+import { Router } from 'express';
+import { UserDetailsBookform } from '../models/bodyPart.js';
+// import { validateInput } from '../middlewares/validationMiddleware.js';
+
+
+const router = Router();
+
+// Common function to handle saving user details
+const saveUserDetails = async (req, res, UserDetailsModel) => {
+    try {
+      const user = new UserDetailsModel(req.body);
+      const savedUser = await user.save();
+      console.log('User details saved successfully:', savedUser);
+      res.json(savedUser);
+    } catch (error) {
+      console.error('Error saving user details:', error);
+      res.status(500).json({ error: error.message });
+    }
+  };
+
+
+// Endpoint for user details from bookform
+router.post('/user-details-bookform', async (req, res) => {
+  await saveUserDetails(req, res, UserDetailsBookform);
+});
+
+
+
+export default router;
+
+
+
+
+
+
+
