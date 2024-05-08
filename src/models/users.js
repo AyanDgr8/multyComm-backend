53fd4fb2c5edeba8e9d53600ce6742773c248365
Ayan <ayandgr5@gmail.com>
2024-05-08 16:00:39 +0530

    users

---
+++ src/models/users.js
@@ -0,0 +1,37 @@
+// src/models/users.js
+
+
+
+import mongoose from 'mongoose';
+
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+    },
+    
+
+    password:{
+        type: String,
+    },
+
+    firstName: { 
+        type: String, 
+    },
+
+    lastName: { 
+        type: String, 
+    },
+
+    phone: { 
+        type: Number, 
+    },
+
+    email: { 
+        type: String, 
+    },
+});
+
+const Users = mongoose.model('Users', userSchema);
+
+export { Users };
