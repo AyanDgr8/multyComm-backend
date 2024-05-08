21e79d0cdc959faf999216b58a8ad2e0d86c15ae
Ayan <ayandgr5@gmail.com>
2024-05-08 14:58:02 +0530

    register file add

--- src/models/register.js
+++ src/models/register.js
@@ -9,37 +9,27 @@ const registerSchema = new mongoose.Schema({
     
     username: {
         type: String,
-        required: [true, "Username is required"],
-        minLength: [6, "Username must be at least 6 characters long"],
-        match: [/^[a-zA-Z0-9!@#$%^&*()-_+=]{6,}$/, "Please enter a valid userId with at least 6 characters consisting of letters, numbers, and special characters (!@#$%^&*()-_+=)"],
     },
     
 
     password:{
         type: String,
-        required: [true, "Password is required"],
     },
 
     firstName: { 
         type: String, 
-        required: [true, "Please enter your First Name"] 
     },
 
     lastName: { 
         type: String, 
-        required: [true, "Please enter your Last Name"] 
     },
 
     phone: { 
-        type: String, 
-        required: [true, "Phone number is required"], 
-        match: [/^\d{10}$/, "Please enter a valid phone number"],
+        type: Number, 
     },
 
     email: { 
         type: String, 
-        required: [true, "Mention your Email address"], 
-        match: [/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/, "Please enter a valid email address"],
     },
 });
 
