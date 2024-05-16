5b8bc6a5378b1307dfe020aef7cfef1afc5aabf7
Ayan <ayandgr5@gmail.com>
2024-05-16 13:44:49 +0530

    api keys

--- src/middlewares/firebase.js
+++ src/middlewares/firebase.js
@@ -7,13 +7,13 @@ import { getAuth as getFirebaseAuth, sendPasswordResetEmail as sendFirebasePassw
 // import { getAuth as getAdminAuth, sendPasswordResetEmail as sendAdminPasswordResetEmail } from "firebase-admin/auth";
 
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_AUTH_DOMAIN",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-    appId: "YOUR_APP_ID",
-    measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyCxBaGWCjE1F7zRUheeXzoHfCLUUYDj6hg",
+  authDomain: "multycomm-e1901.firebaseapp.com",
+  projectId: "multycomm-e1901",
+  storageBucket: "multycomm-e1901.appspot.com",
+  messagingSenderId: "141466163369",
+  appId: "1:141466163369:web:6c0f27bb28ab34514cf1b8",
+  measurementId: "G-SQKXH5GFWM"
 };
 
 // Initialize Firebase SDK
