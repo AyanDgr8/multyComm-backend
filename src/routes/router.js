82c5020fe74140b23781faffe8b01dcac26e0bdb
Ayan Khan <ayandgr5@gmail.com>
2024-05-17 12:25:29 +0530

    addedded

--- src/routes/router.js
+++ src/routes/router.js
@@ -211,7 +211,6 @@ router.post('/forgot-password', async (req, res) => {
 
 
 // ***************************
-
 // Endpoint for sending OTP
 router.post('/send-otp', async (req, res) => {
   try {
@@ -244,10 +243,11 @@ router.post('/send-otp', async (req, res) => {
 
     res.status(200).json({ message: 'OTP sent successfully' });
   } catch (error) {
-    console.error('Error sending OTP:', error);
+    console.error('Error sending OTP:', error);  
     res.status(500).json({ message: 'Server error' });
   }
 });
+
 
 
 
