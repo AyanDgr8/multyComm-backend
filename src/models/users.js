76db0ee1f95b923ee26b5a0bd93a06dcc97e1159
Ayan Khan <ayandgr5@gmail.com>
2024-05-19 23:27:06 +0530

    major changes

--- src/models/users.js
+++ src/models/users.js
@@ -1,9 +1,15 @@
 // src/models/users.js
 
 
-
 import mongoose from 'mongoose';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET || 'default_jwt_secret';
 
 const userSchema = new mongoose.Schema(
     {
@@ -13,6 +19,8 @@ const userSchema = new mongoose.Schema(
         
         password:{
             type: String,
+            required: true,
+            minlength: 6,
         },
 
         firstName: { 
@@ -29,6 +37,12 @@ const userSchema = new mongoose.Schema(
 
         email: { 
             type: String, 
+            unique: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error("Invalid email");
+                }
+            },
         },
 
         dob: {
@@ -38,11 +52,58 @@ const userSchema = new mongoose.Schema(
         gender: {
             type: String,
         },
+        tokens: [
+            {
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        verifytoken: {
+            type: String,
+        },
     },
     {
         timestamps:true
     }
 );
+
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+    if (this.isModified("password") || this.isNew) {
+        this.password = await bcrypt.hash(this.password, 12);
+    }
+    next();
+});
+
+// Generate auth token
+userSchema.methods.generateAuthToken = async function () {
+    try {
+        const token = jwt.sign({ _id: this._id, email: this.email }, JWT_SECRET, {
+            expiresIn: "10m",
+        });
+        this.tokens = this.tokens.concat({ token });
+        await this.save();
+        return token;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
+
+// Generate refresh token
+userSchema.methods.generateRefreshToken = async function () {
+    try {
+        const refreshToken = jwt.sign({ _id: this._id }, JWT_SECRET, {
+            expiresIn: "24h",
+        });
+        this.refreshToken = refreshToken;
+        await this.save();
+        return refreshToken;
+    } catch (error) {
+        throw new Error(error);
+    }
+};
 
 const Users = mongoose.model('Users', userSchema);
 
