705b93f60db6d4faa9b822391a5d004638abd0c7
Ayan Khan <ayandgr5@gmail.com>
2024-05-21 15:31:00 +0530

    netlify

--- src/app.js
+++ src/app.js
@@ -8,20 +8,28 @@ import { errorHandler, notFoundHandler } from './middlewares/errorHandling.js';
 
 const app = express();
 
-
 // CORS configuration
+const allowedOrigins = ['https://multycomm.netlify.app'];
+
 const corsOptions = {
-    origin: 'https://multycomm.netlify.app/', // Change this to the origin of your frontend
-    credentials: true, // Allow credentials
-  };
+  origin: function (origin, callback) {
+    // Allow requests with no origin, like mobile apps or curl requests
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true, // Allow credentials
+  optionsSuccessStatus: 200 // Some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", router);
-
-
 
 // Middleware for handling 404 errors
 app.use(notFoundHandler);
